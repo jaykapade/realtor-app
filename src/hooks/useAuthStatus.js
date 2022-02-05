4eea30f3a8edea1b8dafa30d6ccf4d1ecc401111
jazzdestructor <jazzdestructor@gmail.com>
2022-02-05 18:56:57 +0530

    Fixed a memoryLeak issue

--- src/hooks/useAuthStatus.js
+++ src/hooks/useAuthStatus.js
@@ -1,16 +1,22 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 const useAuthStatus = () => {
   const [loggedIn, setLoggedIn] = useState(false);
   const [checkInStatus, setCheckInStatus] = useState(true);
+  const isMounted = useRef(true);
 
   useEffect(() => {
-    const auth = getAuth();
-    onAuthStateChanged(auth, (user) => {
-      if (user) setLoggedIn(true);
-      setCheckInStatus(false);
-    });
-  }, []);
+    if (isMounted) {
+      const auth = getAuth();
+      onAuthStateChanged(auth, (user) => {
+        if (user) setLoggedIn(true);
+        setCheckInStatus(false);
+      });
+    }
+    return () => {
+      isMounted.current = false;
+    };
+  }, [isMounted]);
 
   return { loggedIn, checkInStatus };
 };
