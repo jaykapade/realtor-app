f45ac03fc65cb228c41332aef180f53227b75545
jazzdestructor <jazzdestructor@gmail.com>
2022-02-06 19:52:22 +0530

    Added CreateListing Page modified Offers Page and ListingItem a bit and some css

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -1,11 +1,10 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import { ReactComponent as DeleteIcon } from "../assets/svg/deleteIcon.svg";
+import { ReactComponent as EditIcon } from "../assets/svg/editIcon.svg";
 import bedIcon from "../assets/svg/bedIcon.svg";
 import bathtubIcon from "../assets/svg/bathtubIcon.svg";
 
-const ListingItem = ({ listing, id, onEdit, onDelete }) => {
-  //   console.log(listing);
+function ListingItem({ listing, id, onEdit, onDelete }) {
   return (
     <li className="categoryListing">
       <Link
@@ -13,13 +12,14 @@ const ListingItem = ({ listing, id, onEdit, onDelete }) => {
         className="categoryListingLink"
       >
         <img
-          src={listing.imageUrls[0]}
+          src={listing.imgUrls[0]}
           alt={listing.name}
           className="categoryListingImg"
         />
         <div className="categoryListingDetails">
           <p className="categoryListingLocation">{listing.location}</p>
           <p className="categoryListingName">{listing.name}</p>
+
           <p className="categoryListingPrice">
             $
             {listing.offer
@@ -33,21 +33,21 @@ const ListingItem = ({ listing, id, onEdit, onDelete }) => {
           </p>
           <div className="categoryListingInfoDiv">
             <img src={bedIcon} alt="bed" />
-            <p className="categoryListingIfoText">
+            <p className="categoryListingInfoText">
               {listing.bedrooms > 1
                 ? `${listing.bedrooms} Bedrooms`
-                : `1 Bedroom`}
+                : "1 Bedroom"}
             </p>
-            &nbsp;
-            <img src={bathtubIcon} alt="bathtub" />
-            <p className="categoryListingIfoText">
+            <img src={bathtubIcon} alt="bath" />
+            <p className="categoryListingInfoText">
               {listing.bathrooms > 1
                 ? `${listing.bathrooms} Bathrooms`
-                : `1 Bathroom`}
+                : "1 Bathroom"}
             </p>
           </div>
         </div>
       </Link>
+
       {onDelete && (
         <DeleteIcon
           className="removeIcon"
@@ -55,8 +55,10 @@ const ListingItem = ({ listing, id, onEdit, onDelete }) => {
           onClick={() => onDelete(listing.id, listing.name)}
         />
       )}
+
+      {onEdit && <EditIcon className="editIcon" onClick={() => onEdit(id)} />}
     </li>
   );
-};
+}
 
 export default ListingItem;
